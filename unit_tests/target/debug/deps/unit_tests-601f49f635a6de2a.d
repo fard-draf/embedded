37c7e52d17f1f789edf9654ff9d1141e17a0d695
fard-draf <fard@ik.me>
2025-07-14 14:21:04 +0200

    Sauvegarde avant refactoring en workspace

--- unit_tests/target/debug/deps/unit_tests-601f49f635a6de2a.d
+++
@@ -1,6 +0,0 @@
-/mnt/repo/dev/warehouse/playground/embedded/unit_tests/target/debug/deps/unit_tests-601f49f635a6de2a.d: src/main.rs src/domain.rs
-
-/mnt/repo/dev/warehouse/playground/embedded/unit_tests/target/debug/deps/libunit_tests-601f49f635a6de2a.rmeta: src/main.rs src/domain.rs
-
-src/main.rs:
-src/domain.rs:
--- unit_tests/target/debug/deps/unit_tests-c2a5d8d5b9781aad.d
+++
@@ -1,6 +0,0 @@
-/mnt/repo/dev/warehouse/playground/embedded/unit_tests/target/debug/deps/unit_tests-c2a5d8d5b9781aad.d: src/main.rs src/domain.rs
-
-/mnt/repo/dev/warehouse/playground/embedded/unit_tests/target/debug/deps/libunit_tests-c2a5d8d5b9781aad.rmeta: src/main.rs src/domain.rs
-
-src/main.rs:
-src/domain.rs:
