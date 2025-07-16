2a24bdcf23b52cafc6f51a4f423748416ce3012d
fard-draf <fard@ik.me>
2025-07-16 06:55:36 +0200

    "wayback before wasm integration"

--- embedded_lab/applications/esp32/hal/dinghy_sail_tools/src/main.rs
+++ embedded_lab/applications/esp32/hal/dinghy_sail_tools/src/main.rs
@@ -9,9 +9,7 @@ mod ports;
 mod parsing;
 mod battery;
 mod utils;
-mod wasm;
 //==================================================================================
-use linked_list_allocator::LockedHeap;
 use crate::display::display_mode_1;
 use crate::display::display_mode_2;
 use crate::display::display_init;
@@ -20,10 +18,6 @@ use crate::domain::DataBrooker;
 use crate::parsing::nmea_parsing_bytes;
 use crate::ports::AdcByteSource;
 use crate::battery::caclutate_battery_voltage;
-use crate::wasm::wasm_init;
-//==================================================================================
-#[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
 //==================================================================================
 use gps_driver::Gps;
 use esp_hal::delay::Delay;
@@ -34,15 +28,10 @@ use esp_hal::{
     prelude::*,
 };
 use esp_println::println;
-use wasmi;
 //==================================================================================
 
 #[entry]
 fn main() -> ! {
-    //==========================================HEAP_ALLOC
-    const HEAP_SIZE: usize = 200 * 1024;
-    static mut HEAP: [u8; HEAP_SIZE] = [0; HEAP_SIZE];
-    unsafe { ALLOCATOR.lock().init(HEAP.as_mut_ptr(), HEAP_SIZE )};
     //==========================================INIT_METAL
     let peripherals = esp_hal::init(esp_hal::Config::default());
     let drivers = board::init(peripherals);
@@ -59,8 +48,6 @@ fn main() -> ! {
     );
     let mut adc1_pin: AdcPin<esp_hal::gpio::GpioPin<34>, peripherals::ADC1> =
         drivers.volt_adc.adc_pin;
-    //==========================================WASM_DISPLAY
-    let mut wasm_init = wasm_init();
     //==========================================LOOP
     loop {
         match adc1_byte_source.read_value_blocking(&mut adc1_pin) {
@@ -76,17 +63,8 @@ fn main() -> ! {
         match gps.update() {
             Ok(gps_data) => {
                 nmea_parsing_bytes(gps_data, &mut data_brooker);
-                wasm_init.layout_func.call(
-                    &mut wasm_init.store, 
-                        (
-                            data_brooker.speed.0.unwrap_or(0.0),
-                            data_brooker.voltage.unwrap_or(0.0),
-                            data_brooker.altitude.unwrap_or(0.0),
-                        )
-                ).unwrap();
-                // let display_setup = display_mode_2(&data_brooker);
-                // generate_layout_func.call(, params)
-                display_print(&mut display, &wasm_init.store.data().layout);
+                let display_setup = display_mode_2(&data_brooker);
+                display_print(&mut display, &display_setup);
                 data_brooker = DataBrooker::default();
             }
             Err("No new data") => {
